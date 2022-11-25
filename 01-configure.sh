6c3ec68390ad406f4f8d0f00df637ad2ab9be9f4
Thirty Eighth and Brunswick <86792483+38B@users.noreply.github.com>
2022-11-25 13:18:37 -0500

    initial configure script

---
+++ 01-configure.sh
@@ -0,0 +1,192 @@
+#!/usr/bin/env bash
+
+set -e
+
+exec &> >(tee "configure.log")
+
+print () {
+    echo -e "\n\033[1m> $1\033[0m\n"
+}
+
+ask () {
+    read -p "> $1 " -r
+    echo
+}
+
+# Tests
+tests () {
+    ls /sys/firmware/efi/efivars > /dev/null && \
+        ping voidlinux.org -c 1 > /dev/null &&  \
+        modprobe zfs &&                         \
+        print "Tests ok"
+}
+
+select_disk () {
+    # Set DISK
+    select ENTRY in $(ls /dev/disk/by-id/);
+    do
+        DISK="/dev/disk/by-id/$ENTRY"
+        echo "$DISK" > /tmp/disk
+        echo "Installing on $ENTRY."
+        break
+    done
+}
+
+wipe () {
+    ask "Do you want to wipe all datas on $ENTRY ?"
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+        # Clear disk
+        dd if=/dev/zero of="$DISK" bs=512 count=1
+        wipefs -af "$DISK"
+        sgdisk -Zo "$DISK"
+    fi
+}
+
+partition () {
+    # EFI part
+    print "Creating EFI part"
+    sgdisk -n1:1M:+512M -t1:EF00 "$DISK"
+    EFI="$DISK-part1"
+
+    # ZFS part
+    print "Creating ZFS part"
+    sgdisk -n3:0:0 -t3:bf01 "$DISK"
+
+    # Inform kernel
+    partprobe "$DISK"
+
+    # Format efi part
+    sleep 1
+    print "Format EFI part"
+    mkfs.vfat "$EFI"
+}
+
+create_zfs_key () {
+    dd if=/dev/urandom of=/etc/keystore/zroot.key bs=32 count=1
+}
+
+create_pool () {
+    # ZFS part
+    ZFS="$DISK-part3"
+
+    # Create ZFS pool
+    print "Create ZFS pool"
+    zpool create -f -o ashift=12                          \
+                 -o autotrim=on                           \
+                 -O acltype=posixacl                      \
+                 -O compression=zstd                      \
+                 -O relatime=on                           \
+                 -O xattr=sa                              \
+                 -O dnodesize=legacy                      \
+                 -O encryption=aes-256-gcm                \
+                 -O keyformat=raw                         \
+                 -O keylocation=file:///etc/keystore/zroot.key \
+                 -O normalization=formD                   \
+                 -O mountpoint=none                       \
+                 -O canmount=off                          \
+                 -O devices=off                           \
+                 -R /mnt                                  \
+                 zroot "$ZFS"
+}
+
+create_root_dataset () {
+    # Slash dataset
+    print "Create root dataset"
+    zfs create -o mountpoint=none                 zroot/ROOT
+
+    # Set cmdline
+    zfs set org.zfsbootmenu:commandline="ro quiet" zroot/ROOT
+}
+
+create_system_dataset () {
+    print "Create slash dataset"
+    zfs create -o mountpoint=/ -o canmount=noauto zroot/ROOT/"$1"
+    print "Generate hostid"
+    zgenhostid
+    print "Set ZFS bootfs"
+    zpool set bootfs="zroot/ROOT/$1" zroot
+    zfs mount zroot/ROOT/"$1"
+}
+
+create_keystore_dataset () {
+    print "Create keystore"
+    zfs create -V 50m zroot/keystore
+    print "Format keystore crypto"
+    cryptsetup luksFormat /dev/zvol/zroot/keystore
+    cryptsetup open --type luks /dev/zvol/zroot/keystore keystore
+    print "Wipe keystore"
+    cp /dev/zero /dev/mapper/keystore
+    print "Format keystore filesystem"
+    mkfs.ext2 /dev/mapper/keystore
+    print "Add keystore to crypttab"
+    echo 'keystore /dev/zvol/zroot/keystore none' >> /etc/crypttab
+    mkdir /etc/keystore
+    echo '/dev/mapper/keystore /etc/keystore ext2 dmask=077,fmask=177,ro 0 2' >> /etc/fstab
+    mount -o rw /etc/keystore
+}
+
+create_home_dataset () {
+    print "Create home dataset"
+    zfs create -o mountpoint=/ -o canmount=off zroot/data
+    zfs create                                 zroot/data/home
+    zfs create -o mountpoint=/root             zroot/data/home/root
+}
+
+export_pool () {
+    print "Export zpool"
+    zpool export zroot
+}
+
+import_pool () {
+    print "Import zpool"
+    zpool import -d /dev/disk/by-id -R /mnt zroot -N -f
+    zfs load-key zroot
+}
+
+mount_system () {
+    print "Mount slash dataset"
+    zfs mount zroot/ROOT/"$1"
+    zfs mount -a
+
+    # Mount EFI part
+    print "Mount EFI part"
+    EFI="$DISK-part1"
+    mkdir -p /mnt/efi
+    mount "$EFI" /mnt/efi
+}
+
+copy_zpool_cache () {
+    # Copy ZFS cache
+    print "Generate and copy zfs cache"
+    mkdir -p /mnt/etc/zfs
+    zpool set cachefile=/etc/zfs/zpool.cache zroot
+}
+
+# Main
+
+tests
+
+select_disk
+
+wipe
+partition
+create_pool
+create_root_dataset
+create_keystore_dataset
+
+ask "Name of the slash dataset ?"
+name_reply="$REPLY"
+echo "$name_reply" > /tmp/root_dataset
+
+create_system_dataset "$name_reply"
+
+create_home_dataset
+
+export_pool
+import_pool
+mount_system "$name_reply"
+copy_zpool_cache
+
+# Finish
+echo -e "\e[32mAll OK"
